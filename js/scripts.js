ce84165f6aa25dabb236170ab56c84c8a96d58c1
Dave Denlow and Levi Bibo <dave@>
2016-10-26 09:13:43 -0700

    program returns when a year is not a leap year.

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,12 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     var year = $("#yearInput").val();
+    console.log(year);
 
-
+    if (getLeapYear(year)) {
+      $(".result").text("is");
+    } else if (!getLeapYear(year)) {
+      $(".result").text("is not");
+    }
   })
 })
