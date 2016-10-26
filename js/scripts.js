268d7357438e8d444cf795d9c53f3b74e599801e
Dave Denlow and Levi Bibo <levi@>
2016-10-26 09:04:27 -0700

    create initial files and directories.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function getLeapYear(year) {
+  return false;
+}
+
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    var year = $("#yearInput").val();
+
+
+  })
+})
